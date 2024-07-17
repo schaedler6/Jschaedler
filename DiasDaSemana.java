d911de21cefcaf930013ade54e2675f0f0218759
Sidinei Roberto Schaedler <166084110+schaedler6@users.noreply.github.com>
2024-07-17 20:55:43 -0300

    Add files via upload

---
+++ DiasDaSemana.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class DiasDaSemana {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita e lê o número do dia da semana
+        System.out.print("Digite um número para o dia da semana (1-7): ");
+        int dia = scanner.nextInt();
+
+        // Verifica e imprime o nome do dia da semana
+        switch (dia) {
+            case 1:
+                System.out.println("O dia é segunda-feira");
+                break;
+            case 2:
+                System.out.println("O dia é terça-feira");
+                break;
+            case 3:
+                System.out.println("O dia é quarta-feira");
+                break;
+            case 4:
+                System.out.println("O dia é quinta-feira");
+                break;
+            case 5:
+                System.out.println("O dia é sexta-feira");
+                break;
+            case 6:
+                System.out.println("O dia é sábado");
+                break;
+            case 7:
+                System.out.println("O dia é domingo");
+                break;
+            default:
+                System.out.println("Número inválido! Por favor, digite um número entre 1 e 7.");
+                break;
+        }
+
+        // Fecha o scanner
+        scanner.close();
+    }
+}
